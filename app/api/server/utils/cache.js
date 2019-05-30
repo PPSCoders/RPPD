352f8c727771940ebb5b12efef3c926ffa130d14
kwangyel <kwangyel@gmail.com>
2019-05-30 11:17:55 +0600

    added vue project for the frontend

--- app/api/server/utils/cache.js
+++ app/api/server/utils/cache.js
@@ -4,27 +4,6 @@ import Util from '../utils/Utils';
 
 const util=new Util();
 
-
-// module.exports = {
-//   /** Koa middleware function to check cache before continuing to any endpoint handlers */
-//   async checkResponseCache (ctx, next) {
-//     const cachedResponse = await redis.get(ctx.path)
-//     if (cachedResponse) { // If cache hit
-//       ctx.body = JSON.parse(cachedResponse) // return the cached response
-//     } else {
-//       await next() // only continue if result not in cache
-//     }
-//   },
-//   /** Koa middleware function to insert response into cache */
-//   async addResponseToCache (ctx, next) {
-//     await next() // Wait until other handlers have finished
-//     if (ctx.body && ctx.status === 200) { // If request was successful
-//       // Cache the response
-//       await redis.set(ctx.path, JSON.stringify(ctx.body))
-//     }
-//   }
-// }
-
 module.exports = {
   /** Koa middleware function to check cache before continuing to any endpoint handlers */
   async checkResponseCache (req,res, next) {
@@ -46,19 +25,3 @@ module.exports = {
     }
   }
 }
-
-// module.exports = {
-//   async checkResponseCache (key) {
-//     const cachedResponse = await redis.get(key)
-//     if (cachedResponse) { // If cache hit
-//       return checkResponseCache; // return the cached response
-//     } else {
-//       return null;
-//     }
-//   },
-//   /** Koa middleware function to insert response into cache */
-//   async addResponseToCache (key,value) {
-//       // Cache the response
-//       return await redis.set(key, JSON.stringify(value));
-//     }
-// }
